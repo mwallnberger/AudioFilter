98418d5464872f8f9bb9340581522844b5a74b21
mwallnberger <martinwallnberger@gmail.com>
2014-03-26 16:31:39 +0100

    .

--- AudioTool/src/AudioTool.java
+++ AudioTool/src/AudioTool.java
@@ -1,10 +1,8 @@
 
-public class AudioTool {
+public class AudioTool 
+{
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
 	}
-
 }
