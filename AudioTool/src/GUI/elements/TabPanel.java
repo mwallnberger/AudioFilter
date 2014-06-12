73c48b006bdf40565e943e4b52fa087de182e7c2
Jakob <Jakob@140.78.187.228>
2014-06-12 14:17:20 +0200

    mario update

---
+++ AudioTool/src/GUI/elements/TabPanel.java
@@ -0,0 +1,60 @@
+package GUI.elements;
+
+import javax.swing.JButton;
+import javax.swing.JTabbedPane;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import GUI.MainWindow;
+import GUI.SignalPanel;
+
+import java.awt.*;
+
+public class TabPanel extends JPanel {
+
+	JTabbedPane panel = new JTabbedPane();
+	ImageIcon closeIcon = new ImageIcon(MainWindow.class.getResource("/fatcow-hosting-icons-3000/16x16/cross.png"));
+
+	
+	public TabPanel() {
+		setLayout(new GridLayout(1, 1));
+		add(panel);
+	}
+	
+	protected JPanel createInnerPanel(String text) {
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.setBackground(Color.white); 
+		
+		SignalPanel chart = new SignalPanel();
+		OptionPanel options = new OptionPanel();
+		
+		panel.add(chart.refresh(), BorderLayout.CENTER);
+		panel.add(options, BorderLayout.LINE_END);
+
+		return panel;
+	}
+	
+	public void createNewTab(String name) {
+		panel.addTab(name, createInnerPanel("test"));
+		int index = panel.indexOfTab(name);
+		
+		//for "x" button in tab
+		JPanel closePanel = new JPanel();
+		JButton closeButton = new JButton(closeIcon);
+		JLabel tabTitle = new JLabel(name);
+		closePanel.setOpaque(false);
+		closeButton.addActionListener(new CloseTabActionHandler(name, panel));
+		closePanel.add(tabTitle);
+		closePanel.add(closeButton);
+		
+		panel.setTabComponentAt(index, closePanel);
+	}
+	
+//	
+//	public static void main(String[] args) {
+//		JFrame frame = new JFrame("TabPane");
+//		frame.getContentPane().add(new TabPane(), BorderLayout.CENTER);
+//		frame.setVisible(true);
+//	}
+}
